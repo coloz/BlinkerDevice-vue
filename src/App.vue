36a5dbf017a428b39a085e01848ec10a02de2be9
Lvzhou <coloz999@gmail.com>
2020-06-04 22:16:13 +0800

    上传

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <BlinkerDevice />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import BlinkerDevice from './components/BlinkerDevice'
+import BlinkerDevice from "./components/BlinkerDevice";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     BlinkerDevice
   }
-}
+};
+let appData = {};
+let showBox = document.getElementById("showBox");
+// window.addEventListener("message", receiveMessage, false);
+
+// function send2Device(data) {
+//   window.parent.postMessage(data, "*");
+// }
+
+// function getState() {
+//   send2Device({ get: "state" });
+// }
+
+// function tapButton() {
+//   if (appData.deviceData.switch == "on") send2Device({ switch: "off" });
+//   else send2Device({ switch: "on" });
+// }
+
+// function receiveMessage(e) {
+//   if (typeof e.data.headerHeight != "undefined") {
+//     let header = document.getElementById("header");
+//     header.style.height = e.data.headerHeight + "px";
+//   }
+//   Object.assign(appData, e.data);
+//   showBox.innerHTML = generateRes(JSON.stringify(appData));
+// }
+// send2Device({});
 </script>
 
 <style>
--- src/components/BlinkerDevice.vue
+++ src/components/BlinkerDevice.vue
@@ -1,42 +1,233 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
+  <div class="device-box">
+    <div class="device-bg"></div>
+    <!-- <div>实时状态</div> -->
+    <div class="state-box">
+      <div class="item block">
+        <div class="title">备连接数</div>
+        <div class="number">7</div>
+      </div>
+      <div class="item block">
+        <div class="title">消息发送条数</div>
+        <div class="number">45</div>
+      </div>
+      <div class="item block">
+        <div class="title">服务运行时间</div>
+        <div class="number">05:13:57</div>
+      </div>
+      <div class="item">
+        <div class="gauge-box gauge-container" id="gauge1">
+          <div class="label">负载</div>
+          <div class="label2">%</div>
+        </div>
+      </div>
+      <div class="item">
+        <div class="gauge-box gauge-container" id="gauge2">
+          <div class="label">CPU</div>
+          <div class="label2">%</div>
+        </div>
+      </div>
+      <div class="item">
+        <div class="gauge-box gauge-container" id="gauge3">
+          <div class="label">内存</div>
+          <div class="label2">%</div>
+        </div>
+      </div>
+    </div>
+    <div class="img-box">
+      <img class="device-img" src="../assets/nuc.png" />
+    </div>
+    <!-- <div class="chart-box">
+      <div>
+        <div>
+          <div>CPU使用率</div>
+        </div>
+        <div>内存使用率</div>
+        <div>CPU使用率</div>
+        <div>消息发送条数</div>
+      </div>
+    </div>-->
+    <div class="switch" v-on:click="clickSwitch">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path
+          d="M388.5 46.3C457.9 90.3 504 167.8 504 256c0 136.8-110.8 247.7-247.5 248C120 504.3 8.2 393 8 256.4 7.9 168 54 90.3 123.5 46.3c5.8-3.7 13.5-1.8 16.9 4.2l3.9 7c3.1 5.6 1.3 12.6-4.1 16C79.9 112 40 179.6 40 256c0 119.9 97.3 216 216 216 119.9 0 216-97.3 216-216 0-77-40.1-144.2-100.3-182.4-5.4-3.4-7.2-10.5-4.1-16l3.9-7c3.4-6.1 11.2-7.9 17-4.3zM272 276V12c0-6.6-5.4-12-12-12h-8c-6.6 0-12 5.4-12 12v264c0 6.6 5.4 12 12 12h8c6.6 0 12-5.4 12-12z"
+        />
+      </svg>
+    </div>
   </div>
 </template>
 
 <script>
+import "../blinker";
+var Gauge = require("svg-gauge");
 export default {
-  name: 'BlinkerDevice',
+  name: "BlinkerDevice",
   props: {
-    msg: String
+    deviceId: String
+    // data: objec
+  },
+  mounted: function() {
+    Gauge(document.getElementById("gauge1"), {
+      max: 100,
+      value: 41,
+      valueClass: "value-lable",
+      color: value => {
+        if (value < -25) {
+          return "#5ee432";
+        } else if (value < 0) {
+          return "#fffa50";
+        } else if (value < 25) {
+          return "#f7aa38";
+        } else {
+          return "#ef4655";
+        }
+      }
+    });
+    Gauge(document.getElementById("gauge2"), {
+      max: 100,
+      value: 45,
+      valueClass: "value-lable"
+    });
+    Gauge(document.getElementById("gauge3"), {
+      max: 100,
+      value: 50,
+      valueClass: "value-lable"
+    });
+  },
+  methods: {
+    clickSwitch: function(event) {
+      console.log(event);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+.device-box {
+  color: #fff;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.device-bg {
+  margin: 0;
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: radial-gradient(
+    circle,
+    rgb(107, 107, 107),
+    rgb(42, 51, 58),
+    rgb(4, 9, 14)
+  );
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.img-box {
+  margin-top: 100px;
 }
-a {
-  color: #42b983;
+.device-img {
+  width: 60vw;
+}
+.state-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  font-size: 12px;
+  color: #999999;
+  .item {
+    margin-top: 50px;
+    width: 28vw;
+    height: 8vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    // border: 1px solid #ccc;
+    flex-direction: column;
+    .gauge-box {
+      width: 100%;
+      position: relative;
+      .label {
+        font-size: 12px;
+        font-weight: 100;
+        width: 100%;
+        text-align: center;
+        position: absolute;
+        top: 22%;
+        color: rgb(153, 153, 153);
+        display: inline-block;
+      }
+      .label2 {
+        font-size: 18px;
+        font-weight: 100;
+        width: 100%;
+        text-align: center;
+        position: absolute;
+        bottom: 23%;
+        color: #fff;
+        display: inline-block;
+      }
+    }
+    .gauge-container {
+      svg.gauge {
+        path.dial {
+          stroke: #676e75;
+          stroke-width: 5;
+        }
+      }
+    }
+    .gauge-container {
+      .gauge .value {
+        stroke: #f8774b;
+        stroke-dasharray: 25 1;
+        stroke-width: 5;
+      }
+    }
+    .gauge-container {
+      .gauge .value-lable {
+        fill: #fff;
+        font-size: 1.5em;
+      }
+    }
+  }
+}
+
+.value-lable {
+  font-size: 20px;
+  // font-weight: bold;
+  color: #fff;
+  margin-top: 5px;
+}
+
+.block {
+  height: 18vh;
+  margin-top: 0 !important;
+  .title {
+    border-bottom: 1px solid rgb(155, 155, 155);
+    padding-bottom: 6px;
+  }
+  .number {
+    margin-top: 5px;
+    font-size: 20px;
+    color: #fff;
+  }
+}
+
+.chart-box {
+  display: flex;
+}
+
+.switch {
+  position: absolute;
+  left: calc(50% - 14vw);
+  bottom: 50px;
+  border-radius: 50%;
+  border: 1px solid rgb(77, 77, 77);
+  width: 14vw;
+  height: 14vw;
+  padding: 5vw;
+  box-shadow: 0 0 10px rgb(194, 194, 194);
+  svg {
+    fill: #fff;
+  }
 }
 </style>
